87375eb7263c6b086aca39eacd89ad0930635d2e
ThomasLaspas <makislaspas@gmail.com>
2024-05-06 12:25:24 +0300

    delete

--- src/pages/myrest.jsx
+++ src/pages/myrest.jsx
@@ -2,9 +2,31 @@ import { useState } from "react";
 import "../mainpage.css";
 import Upade from "../components/updateres";
 import Restorders from "../components/restoredrs";
+import { server } from "../util/axios";
 
 export default function Myrest() {
   const [btnac, setbtn] = useState(true);
+  const Token = localStorage.getItem("tok");
+  const username = localStorage.getItem("name");
+  const delt = async () => {
+    try {
+      const res = await server.delete(
+        `api/restaurant/delete/${username}`,
+
+        {
+          headers: {
+            Authorization: `Bearer ${Token}`,
+          },
+        }
+      );
+      localStorage.setItem("ad", false);
+
+      alert("your estaurant deleted sucesfully");
+      window.location.href = "/";
+    } catch (err) {
+      alert("some error ocured try again later");
+    }
+  };
 
   const display = () => {
     setbtn((prev) => !prev);
@@ -20,7 +42,9 @@ export default function Myrest() {
             Update Restaurant
           </button>
         </div>
-        <button className="deletebtn">Delete Restaurant</button>
+        <button className="deletebtn" onClick={delt}>
+          Delete Restaurant
+        </button>
       </header>
       <br />
       <div className="forms">{btnac ? <Restorders /> : <Upade />}</div>
